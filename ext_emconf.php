92fcfafd417a8cf21f783bad774a5bef8491a486
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-30 17:01:46 +0000

    [TASK] Release of version 0.9.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/menu_balancer/trunk@52596 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "menu_balancer".
 #
-# Auto generated 29-09-2011 21:40
+# Auto generated 30-09-2011 19:01
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Oliver Hader',
 	'author_email' => 'oliver.hader@typo3.org',
 	'shy' => '',
-	'dependencies' => 'cms',
+	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -29,17 +29,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.8.0',
+	'version' => '0.9.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-',
+			'typo3' => '4.5.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:10:"README.txt";s:4:"e3c4";s:16:"ext_autoload.php";s:4:"214d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"367d";s:20:"Classes/Renderer.php";s:4:"1bd2";s:36:"Classes/Model/AbstractCollection.php";s:4:"34d1";s:40:"Classes/Model/BalancedPartCollection.php";s:4:"a2bd";s:31:"Classes/Model/Configuration.php";s:4:"9541";s:22:"Classes/Model/Part.php";s:4:"731c";s:32:"Classes/Model/PartCollection.php";s:4:"df0f";s:28:"Classes/Service/Balancer.php";s:4:"1595";s:28:"Classes/Service/Splitter.php";s:4:"3cd8";s:34:"Configuration/TypoScript/setup.txt";s:4:"3e29";}',
+	'_md5_values_when_last_written' => 'a:13:{s:10:"README.txt";s:4:"e3c4";s:16:"ext_autoload.php";s:4:"214d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"367d";s:20:"Classes/Renderer.php";s:4:"05a9";s:36:"Classes/Model/AbstractCollection.php";s:4:"e5ac";s:40:"Classes/Model/BalancedPartCollection.php";s:4:"74dc";s:31:"Classes/Model/Configuration.php";s:4:"5999";s:22:"Classes/Model/Part.php";s:4:"deea";s:32:"Classes/Model/PartCollection.php";s:4:"9f6c";s:28:"Classes/Service/Balancer.php";s:4:"7843";s:28:"Classes/Service/Splitter.php";s:4:"9e36";s:34:"Configuration/TypoScript/setup.txt";s:4:"3e29";}',
 	'suggests' => array(
 	),
 );
